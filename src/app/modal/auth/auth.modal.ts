c4bc60a4191bb065f8e953e4b25b164118f3f00f
Roman Polishchuk <roman.polishchuk@happytech.io>
2024-01-04 17:13:16 +0600

    auth

--- src/app/modal/auth/auth.modal.ts
+++ src/app/modal/auth/auth.modal.ts
@@ -33,9 +33,89 @@ export class AuthModal implements OnInit {
     }
   }
 
+  /*async signInWithApple() {
+    try {
+      this.appService.showLoader();
+      const token = await this.auth.signInWithApple();
+      if (token) {
+        this.token = token;
+        await this.onAuthSuccess();
+      }
+    } catch (err: any) {
+      console.error('failed to sign in', err);
+    } finally {
+      this.appService.hideLoader();
+    }
+  }
+
+  async signInWithGoogle() {
+    this.loading.google = true;
+    try {
+      this.appService.showLoader();
+      const token = await this.auth.signInWithGoogle();
+      if (token) {
+        this.token = token;
+        await this.onAuthSuccess();
+      }
+    } catch (err: any) {
+      console.error('failed to sign in', err);
+    } finally {
+      this.loading.google = false;
+      this.appService.hideLoader();
+    }
+  }*/
+
   async modalDismiss() {
     await this.router.navigateByUrl('main');
     await this.modalCtrl.dismiss();
 
   }
 }
+
+/*
+
+async signInWithGoogle(): Promise<string | undefined> {
+  const user = await GoogleAuth.signIn();
+  return firstValueFrom(this.authResource.signInWithGoogle({
+    identityToken: user.authentication.idToken,
+    mobile: AppService.isMobile(),
+  })).then(res => res.success ? res.token : undefined);
+}
+
+async signInWithApple(): Promise<string | undefined> {
+  const {nonce} = await this.getAppleNonce();
+  const token = (await SignInWithApple.authorize({
+    clientId: 'com.happycake.store.signin',
+    redirectURI: 'https://happycake.kz/login',
+    scopes: 'email name',
+    state: 'hc_app',
+    nonce: nonce,
+  })).response.identityToken;
+
+  return firstValueFrom(this.authResource.signInWithApple({
+    identityToken: token,
+    mobile: AppService.isMobile(),
+  })).then(res => res.success ? nonce : undefined);
+}
+
+private async onAuthSuccess() {
+  if (AppService.isMobile()) {
+    if (!this.token) {
+      throw new Error('[auth] Token is undefined');
+    }
+    await this.auth.saveToken(this.token);
+  }
+  const authUser = await this.auth.getMe();
+  this.auth.userUpdated(authUser);
+  if (this.auth.postUrl) {
+    await this.router.navigateByUrl(this.auth.postUrl);
+    this.auth.postUrl = undefined;
+  } else {
+    const city = await this.storageService.getCity();
+    if (city) {
+      await this.router.navigateByUrl(AppService.isMobile() ? '/main' : ('/' + city.slug));
+    } else {
+      await this.router.navigateByUrl('/cities');
+    }
+  }
+}*/
